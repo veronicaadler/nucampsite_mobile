ce4d31c9efdce09de0795d249109c551c4409429
Veronica Adler <vlynnadler@gmail.com>
2021-09-14 17:31:52 -0400

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet,
-    Picker, Switch, Button } from 'react-native';
+    Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
-import { Alert } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
     constructor(props) {
@@ -37,8 +37,10 @@ class Reservation extends Component {
                 },
                 {
                     text: 'Ok',
-                    onPress: () => this.handleReservation()
-
+                    onPress: () => {
+                    this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                    this.resetForm()
+                    }
                 }
             ]
 
@@ -57,6 +59,31 @@ class Reservation extends Component {
             date: new Date(),
             showCalendar: false,  
         })
+    }
+
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true 
+                })
+            })
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
     }
 
     render() {
